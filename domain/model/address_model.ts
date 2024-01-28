7da2cd67ce340636b2062bcbe5c09a3c7aa3ad68
gminiy <gmini.y@gmail.com>
2024-01-28 22:52:53 +0900

    fix: delete created_at, updated_at from address_model

--- domain/model/address_model.ts
+++ domain/model/address_model.ts
@@ -1,8 +1,3 @@
 export class AddressModel {
-  constructor(
-    public id: string,
-    public address: string,
-    public created_at: number,
-    public updated_at: number
-  ) {}
+  constructor(public id: string, public address: string) {}
 }
